8f29b955b2539727687a515d258af5dc99d2da49
tptnhanan2001 <137023784+tptnhanan2001@users.noreply.github.com>
2025-08-12 16:19:11 +0700

    Update sql_generatecmd.py

--- sql_generatecmd.py
+++ sql_generatecmd.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from burp import IBurpExtender, IContextMenuFactory, IParameter
 from java.util import ArrayList
-from javax.swing import JMenuItem, JOptionPane
+from javax.swing import JMenuItem
 from java.awt.datatransfer import StringSelection
 from java.awt import Toolkit
 import urllib
@@ -13,7 +13,6 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         self._helpers = callbacks.getHelpers()
         callbacks.setExtensionName("SQLMap Command Generator")
         callbacks.registerContextMenuFactory(self)
-        return
 
     def createMenuItems(self, invocation):
         self._invocation = invocation
@@ -22,39 +21,43 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         return menuList
 
     def generateSqlmapCmd(self, event):
-        from javax.swing import JOptionPane  # import ngay đầu hàm, tránh lỗi UnboundLocalError
-        
         httpRequestResponse = self._invocation.getSelectedMessages()
         if not httpRequestResponse or len(httpRequestResponse) == 0:
             return
-        
-        req = httpRequestResponse[0].getRequest()
-        analyzedRequest = self._helpers.analyzeRequest(req)
-        
+
+        req_bytes = httpRequestResponse[0].getRequest()
+        analyzedRequest = self._helpers.analyzeRequest(httpRequestResponse[0])
         method = analyzedRequest.getMethod()
-        
-        url = self._helpers.analyzeRequest(httpRequestResponse[0]).getUrl()
-        if url is None:
-            return
-        url = url.toString()
-        
+        url = analyzedRequest.getUrl().toString()
+
         params = analyzedRequest.getParameters()
-        
+        headers = analyzedRequest.getHeaders()
+
         base_cmd = 'sqlmap -u "{}"'.format(url)
-        
+
         data_params = []
         cookie_params = []
+        extra_headers = []
 
+        # Lấy các param từ body/cookie
         for p in params:
             ptype = p.getType()
             pname = p.getName()
             pvalue = p.getValue()
-            if ptype == IParameter.PARAM_URL:
-                pass
-            elif ptype == IParameter.PARAM_BODY:
+            if ptype == IParameter.PARAM_BODY:
                 data_params.append("{}={}".format(pname, urllib.quote_plus(pvalue)))
             elif ptype == IParameter.PARAM_COOKIE:
                 cookie_params.append("{}={}".format(pname, pvalue))
+
+        # Lấy Authorization/Cookie từ headers
+        for h in headers:
+            h_lower = h.lower()
+            if h_lower.startswith("authorization:"):
+                extra_headers.append(h)
+            elif h_lower.startswith("cookie:") and not cookie_params:
+                # Nếu cookie chưa được lấy từ param thì lấy từ header
+                cookie_value = h[len("Cookie: "):]
+                base_cmd += ' --cookie="{}"'.format(cookie_value)
 
         if method.upper() == "POST":
             base_cmd += " --method=POST"
@@ -67,8 +70,13 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         if cookie_params:
             base_cmd += ' --cookie="{}"'.format('; '.join(cookie_params))
 
-        base_cmd += ' --batch'
+        for h in extra_headers:
+            base_cmd += ' --header="{}"'.format(h)
 
+        base_cmd += ' --batch --level=5 --risk=3 --force-ssl'
+
+        # Copy vào clipboard
         clipboard = Toolkit.getDefaultToolkit().getSystemClipboard()
         clipboard.setContents(StringSelection(base_cmd), None)
 
+        print("[SQLMap CMD] " + base_cmd)
